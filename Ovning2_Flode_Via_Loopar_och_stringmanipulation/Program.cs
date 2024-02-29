f60a2b46fd5444793940913a9012495382119558
FelixEdenborgh <Felix@edenborgh.se>
2024-02-29 18:15:05 +0100

    Add project files.

---
+++ Ovning2_Flode_Via_Loopar_och_stringmanipulation/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Ovning2_Flode_Via_Loopar_och_stringmanipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till denna bio \nHär finns en massa bra filmer kom och skriv in er!\nVem vet ni kanske kan få det biligare!");
+            Console.WriteLine();
+            Console.WriteLine("I denna biljet automat så ser ni en huvudmenyn. \nFör att kunna navigera runt i denna huvudmenyn behöver ni trycka på respektive siffra som hör till respektive meny.");
+            Console.WriteLine();
+
+            
+            bool exit = false;
+            string userInput = string.Empty;
+            string ageOfUser = string.Empty;
+            string howmany = string.Empty;
+            string companyages = string.Empty;
+
+
+
+            // while loop som jobbar så länge exit inte är lika med true
+            while (exit != true) 
+            {
+                // menyn
+                Console.WriteLine("Huvudmeny: ");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Age check");
+                Console.WriteLine("2. Pris för sälskap 2 personer+");
+
+
+                // tar in användar input i string
+                userInput = Console.ReadLine();
+
+                // omvandlar string inputen och testar att göra den till en int, om det inte går så skickar den ett fel meddelande och man får försöka igen.
+                if(int.TryParse(userInput, out int userChoice))
+                {
+                    // switch som sköter om user input dvs i detta laget "userChoice"
+                    switch (userChoice)
+                    {
+                        case 0:
+                            exit = true;
+                            break;
+
+                        case 1:
+                            // tar in en string och kollar så att den inte består av bokstäver utan siffror
+                            Console.WriteLine("Ange older: ");
+                            ageOfUser = Console.ReadLine();
+                            // omvandlar strängen till int om det går annars går till else och skriver "fel input testa igen"
+                            if (int.TryParse(ageOfUser, out int age))
+                            {
+                                Console.WriteLine($"Du är av åldern {age} och ditt biljet pris är {ageCheck.CheckAge(age)}");
+                                
+                            }
+                            else { Console.WriteLine("Fel input testa igen!"); }
+                            break;
+
+                        case 2:
+                            // kollar hur många de är i sälskapet
+                            Console.WriteLine("Ange hur många ni är: ");
+                            howmany = Console.ReadLine();
+                            // kollar om det är en sträng eller int, och om det går att omvandla till int.
+                            if(int.TryParse(howmany, out int howmanyChoice))
+                            {
+                                Console.WriteLine($"Din grupps på {howmanyChoice} personer har en total kostnad på: {ageCheck.GroupPrice(howmanyChoice)}kr");
+                            }
+                            else { Console.WriteLine("Fel input testa igen!"); }
+                            break;
+
+                        default:
+                            Console.WriteLine("Felaktig input");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Voops det där var en text string och inte en siffra. Försök igen!");
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ Ovning2_Flode_Via_Loopar_och_stringmanipulation/ageCheck.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning2_Flode_Via_Loopar_och_stringmanipulation
+{
+    internal class ageCheck
+    {
+        public static int CheckAge(int age)
+        {
+            // 3 ints med ett världe på dem var
+            int ungdomsPris = 80;
+            int pensionärPris = 90;
+            int standartPris = 120;
+
+            int biljetPris = 0;
+
+            // kollar om åldern är under 20 och över 5, om det är så så räknas personen till "ungdom" och får betala ungdomspriset
+            if((age < 20) && (age > 5))
+            {
+                biljetPris = ungdomsPris;
+            }
+            // Kollar om åldern är störe eller lika med 20 och lägre eller lika med 64, och isåfall är det standardpris
+            else if((age >= 20) && (age <= 64))
+            {
+                biljetPris = standartPris;
+            }
+            // Kollar om åldern är störe än 64 och mindre än 100, och om det är så är det pensionärspris.
+            else if((age > 64) && (age <= 100))
+            {
+                biljetPris = pensionärPris;
+            }
+            // är Personen mindre än 5, och störe än 100 så är det gratis inträdde.
+            else
+            {
+                biljetPris = 0;
+            }
+            // skickar tillbaka biljetpriset
+            return biljetPris;
+        }
+
+        public static int GroupPrice(int howmany)
+        {
+            // skapa en lista som sparar in ints på varje persons ålder
+            List<int> list = new List<int>();
+
+            string input = string.Empty;
+            int age = 0;
+            int sum = 0;
+
+            // håller koll på hur många gånger den ska fråga personen
+            Console.WriteLine("Hur gamla är personerna?");
+            for (int i = 1; i <= howmany; i++)
+            {
+                Console.WriteLine($"Person {i}: ");
+                // kollar så att det är en sträng som går att omvandla till int och inget annat.
+                input = Console.ReadLine();
+                if (int.TryParse(input, out age))
+                {
+                    list.Add(age);
+                }
+                else { Console.WriteLine("Fel inmatning här tar vi bara siffror!"); }
+                
+            }
+            
+            // Loopar igenom listan och adderar priset genom att få priset från checkAge metoden
+            foreach(int item in list)
+            {
+                sum += CheckAge(item);
+            }
+
+            // skickar tillbaka gruppens kostnad
+            return sum;
+            
+
+        }
+    }
+}
