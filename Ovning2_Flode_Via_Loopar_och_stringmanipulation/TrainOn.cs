ed6f792e9945500f3e308596794001535e416221
FelixEdenborgh <Felix@edenborgh.se>
2024-03-01 10:26:08 +0100

    Vad jag vill tränna på

---
+++ Ovning2_Flode_Via_Loopar_och_stringmanipulation/TrainOn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning2_Flode_Via_Loopar_och_stringmanipulation
+{
+    internal class TrainOn
+    {
+        /*
+         Vad jag vill tränna på:
+        1. jag vill gärna träna mer på hur man kan göra det jag har skrivit hitils bättre. Då det är fuktionällt men är helt säker på att det kan skrivas
+        mycket snyggare.
+
+        2. Vill gärna tränna mer på void, och ikee void. Vad som ska vara vart och när. Vet att void returnerar inget medans int t ex ska returnera en int.
+
+        3. Vill tränna mer på hur man kan få till en mer OOP kod.
+
+        4. Vill tränna mer på switches då de är lite knepiga i vissa situationer och kan bli lätt fula med hur dem + datat ser ut.
+
+        5. Vill gärna tränna mer på hur man skriver snygg C# kod. 
+
+        6. Lite mer arrays och listor.
+         
+         
+         
+         */
+    }
+}
